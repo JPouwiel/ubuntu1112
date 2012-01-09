42919ce80b7001519346fdbef6307c9b2189eda0
Jeroen Pouwiel <jeroen@pouwiel.com>
2012-01-09 11:48:03 +0100

    Added a User factory and tests for the show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Jeroen Pouwiel"
+  user.email                 "Jeroen@Pouwiel.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
